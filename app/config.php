0f45f11d6a3c1490e18bb45857de50cd85fdb066
FilipRebizant <filiprebizant@gmail.com>
2019-03-22 09:29:01 +0100

    Add config.php file

---
+++ app/config.php
@@ -0,0 +1,14 @@
+define('APP_NAME', 'upskilling');
+define('APP_DOMAIN', '127.0.0.1');
+define('APP_INNER_DIRECTORY', '/mix/mvc');
+define('APP_ROOT', __DIR__.'/..');
+
+define('APP_CONTROLLER_NAMESPACE', 'Controller\\');
+define('APP_DEFAULT_CONTROLLER', 'HomeController');
+define('APP_DEFAULT_CONTROLLER_METHOD', 'index');
+define('APP_CONTROLLER_METHOD_SUFFIX', 'Method');
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'mydb');
+define('DB_USER', 'myuser');
+define('DB_PASS', 'secret');
