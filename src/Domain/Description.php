819b35ecd464fa731abd16c2052ace115e2b46ec
FilipRebizant <filiprebizant@gmail.com>
2019-04-01 13:13:46 +0200

    Create description value object

---
+++ src/Domain/Description.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Domain;
+
+use InvalidArgumentException;
+
+final class Description
+{
+    /** @var string */
+    private $description;
+
+    /**
+     * Description constructor.
+     * @param string $description
+     * @throws InvalidArgumentException
+     */
+    public function __construct(string $description)
+    {
+        if (empty($description)) {
+            throw new InvalidArgumentException("Description can not be empty.");
+        }
+
+        $this->description = $description;
+    }
+}
