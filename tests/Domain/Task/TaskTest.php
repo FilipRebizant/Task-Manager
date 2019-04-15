940de58dcf7f12f9da5368e1d8d33e5aad46e039
FilipRebizant <filiprebizant@gmail.com>
2019-04-15 13:34:34 +0200

    Fixed current task tests

--- tests/Domain/Task/TaskTest.php
+++ tests/Domain/Task/TaskTest.php
@@ -2,19 +2,17 @@
 
 namespace App\Tests\Domain\Task;
 
-
 use App\Domain\Task\Task;
-
+use App\Domain\Task\ValueObject\Description;
 use App\Domain\Task\ValueObject\Priority;
 use App\Domain\Task\ValueObject\Status;
+use App\Domain\Task\ValueObject\Title;
 use App\Domain\User\User;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
-use Ramsey\Uuid\Uuid;
 
 class TaskTest extends TestCase
 {
-
     /** @var User */
     protected $user;
 
@@ -27,13 +25,13 @@ class TaskTest extends TestCase
     public function setUp(): void
     {
         $this->task = new Task(
-            $this->getMockBuilder(Uuid::class)->disableOriginalConstructor()->getMock(),
+            $this->getMockBuilder(Title::class)->disableOriginalConstructor()->getMock(),
             $this->getMockBuilder(Status::class)->disableOriginalConstructor()->getMock(),
             $this->getMockBuilder(User::class)->disableOriginalConstructor()->getMock(),
             $this->getMockBuilder(Priority::class)->disableOriginalConstructor()->getMock(),
-            'Description'
+            $this->getMockBuilder(Description::class)->disableOriginalConstructor()->getMock()
         );
-        $this->userMock = $this->getMockBuilder(User::class)->getMock();
+        $this->userMock = $this->getMockBuilder(User::class)->disableOriginalConstructor()->getMock();
     }
 
     public function testAssignTaskIsToDo()
@@ -47,7 +45,7 @@ class TaskTest extends TestCase
 
     public function testAssignTaskIsPending()
     {
-        $this->task->setPending();
+        $this->task->updateStatus(new Status('Pending'));
         $status = $this->task->getStatus();
         $expectedStatus = "Pending";
 
@@ -56,7 +54,7 @@ class TaskTest extends TestCase
 
     public function testAssignTaskIsDone()
     {
-        $this->task->setDone();
+        $this->task->updateStatus(new Status('Done'));
         $status = $this->task->getStatus();
         $expectedStatus = "Done";
 
@@ -92,7 +90,7 @@ class TaskTest extends TestCase
 
     public function testTaskWillNotUnassignWrongUser()
     {
-        $wrongUserMock = $this->getMockBuilder(User::class)->getMock();
+        $wrongUserMock = $this->getMockBuilder(User::class)->disableOriginalConstructor()->getMock();
 
         $this->task->assignUser($this->userMock);
         $this->task->unassignUser($wrongUserMock);
@@ -103,7 +101,7 @@ class TaskTest extends TestCase
 
     public function testTaskWillNotBeReassigned()
     {
-        $wrongUserMock = $this->getMockBuilder(User::class)->getMock();
+        $wrongUserMock = $this->getMockBuilder(User::class)->disableOriginalConstructor()->getMock();
 
         $this->task->assignUser($this->userMock);
 
