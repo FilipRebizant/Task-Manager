388b43442a84a6b051bf56182998dfc655e31f75
FilipRebizant <filiprebizant@gmail.com>
2019-05-08 11:11:00 +0200

    Routing refactor

--- public/index.php
+++ public/index.php
@@ -9,30 +9,29 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Routing\Loader\YamlFileLoader;
 
 require_once '../vendor/autoload.php';
+$container = require_once __DIR__ . '/../config/dependency_injection.php';
 
-    $container = require_once __DIR__ . '/../config/dependency_injection.php';
+$fileLocator = new FileLocator(__DIR__ . '/../config');
+$loader = new YamlFileLoader($fileLocator);
+$routes = $loader->load('routes.yaml');
 
-    $fileLocator = new FileLocator(__DIR__ . '/../config/');
-    $loader = new YamlFileLoader($fileLocator);
-    $routes = $loader->load('routes.yaml');
+$request = Request::createFromGlobals();
+$context = new RequestContext();
 
-    $request = Request::createFromGlobals();
-    $context = new RequestContext();
+$context->fromRequest($request);
 
-    $context->fromRequest($request);
+$matcher = new UrlMatcher($routes, $context);
 
-    $matcher = new UrlMatcher($routes, $context);
+$request->attributes->add($matcher->match($request->getPathInfo()));
 
-    $request->attributes->add($matcher->match($request->getPathInfo()));
+$method = $request->attributes->get(['_controller'][0]);
 
-    $method = $request->attributes->get(['_controller'][0]);
-
-    $response = $container->call($method, ['request' => $request]);
+$response = $container->call($method, ['request' => $request]);
 try{
 } catch (ResourceNotFoundException $e) {
     $response = new Response('Not found', 404);
 } catch (\Exception $e) {
-    $response = new Response('An error occurred', 500);
+    $response = new Response('An error occurredd', 500);
 }
 
 $response->send();
