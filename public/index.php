d48941a50082bb2b27c66343677f5ab6826ba574
FilipRebizant <filiprebizant@gmail.com>
2019-05-09 13:25:11 +0200

    Changed endpoint type

--- public/index.php
+++ public/index.php
@@ -1,6 +1,8 @@
 <?php
 
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Exception\MethodNotAllowedException;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
@@ -10,28 +12,30 @@ use Symfony\Component\Routing\Loader\YamlFileLoader;
 
 require_once '../vendor/autoload.php';
 $container = require_once __DIR__ . '/../config/dependency_injection.php';
-
-$fileLocator = new FileLocator(__DIR__ . '/../config');
-$loader = new YamlFileLoader($fileLocator);
-$routes = $loader->load('routes.yaml');
-
-$request = Request::createFromGlobals();
-$context = new RequestContext();
-
-$context->fromRequest($request);
-
-$matcher = new UrlMatcher($routes, $context);
-
-$request->attributes->add($matcher->match($request->getPathInfo()));
-
-$method = $request->attributes->get(['_controller'][0]);
-
-$response = $container->call($method, ['request' => $request]);
 try{
+
+    $fileLocator = new FileLocator(__DIR__ . '/../config');
+    $loader = new YamlFileLoader($fileLocator);
+    $routes = $loader->load('routes.yaml');
+
+    $request = Request::createFromGlobals();
+    $context = new RequestContext();
+
+    $context->fromRequest($request);
+
+    $matcher = new UrlMatcher($routes, $context);
+
+    $request->attributes->add($matcher->match($request->getPathInfo()));
+
+    $method = $request->attributes->get(['_controller'][0]);
+
+    $response = $container->call($method, ['request' => $request]);
 } catch (ResourceNotFoundException $e) {
-    $response = new Response('Not found', 404);
+    $response = new Response('Page not found.', 404);
+} catch (MethodNotAllowedException $e) {
+    $response = new JsonResponse(['error' => 'Http method not supported.'], 405);
 } catch (\Exception $e) {
-    $response = new Response('An error occurredd', 500);
+    $response = new Response('An error occurred.', 500);
 }
 
 $response->send();
