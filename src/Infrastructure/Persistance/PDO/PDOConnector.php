1bb77ea6718c62f8d327334995f44046c867bbfa
FilipRebizant <filiprebizant@gmail.com>
2019-05-10 12:32:27 +0200

    Update travis settings

--- src/Infrastructure/Persistance/PDO/PDOConnector.php
+++ src/Infrastructure/Persistance/PDO/PDOConnector.php
@@ -22,7 +22,7 @@ class PDOConnector
         ];
 
         try {
-            var_dump(getenv('DB_HOST'));
+//            var_dump(getenv('DB_HOST'));
             $this->connection = new PDO(
                 'mysql:host=' . getenv('DB_HOST') . ';dbname=' . getenv('DB_NAME'),
                 getenv('DB_USER'),
