fd2a58c6ea8b89f88bd673d78e2842e241459f85
FilipRebizant <filiprebizant@gmail.com>
2019-05-15 08:32:51 +0200

    Using cached docker volumes

--- src/Interfaces/Web/Controller/TaskController.php
+++ src/Interfaces/Web/Controller/TaskController.php
@@ -2,12 +2,25 @@
 
 namespace App\Interfaces\Web\Controller;
 
+use App\Infrastructure\Persistance\PDO\User\UserQuery;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TaskController extends AbstractController
 {
+    /** @var UserQuery */
+    private $userQuery;
+
+    public function __construct(UserQuery $userQuery)
+    {
+        $this->userQuery = $userQuery;
+    }
+
     public function index()
     {
-        return $this->render('tasks/index.html.twig');
+        $users = $this->userQuery->getAll();
+
+        var_dump($users);
+
+        return $this->render('tasks/index.html.twig', ['users' => $users]);
     }
 }
