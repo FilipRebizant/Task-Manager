cb0560ee8ddddf8ec315a2046859b4db44eef812
FilipRebizant <filiprebizant@gmail.com>
2019-06-24 10:07:24 +0200

    Install react

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,22 @@
 require('../css/app.scss');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+
+        };
+        console.log('react');
+    }
+
+    render() {
+        return (
+            <div>test</div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
