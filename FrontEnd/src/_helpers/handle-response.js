9f348f4fdcbed9c8f9154f98e67acee72c60684b
FilipRebizant <filiprebizant@gmail.com>
2019-07-18 13:46:48 +0200

    Refresh token improvement

--- FrontEnd/src/_helpers/handle-response.js
+++ FrontEnd/src/_helpers/handle-response.js
@@ -6,10 +6,15 @@ export function handleResponse(response) {
 
         if ([401, 403].indexOf(response.code) !== -1 || response.error && [400, 404].indexOf(response.error.status) !== -1) {
             if (response.message === "Expired JWT Token") {
-                authenticationService.refreshToken(authenticationService.currentUserValue.username).then(() => {
-                    location.reload(true);
-                }
-                );
+                let refresh = function () {
+                    if (refresh.done) return;
+
+                    authenticationService.refreshToken(authenticationService.currentUserValue.username).then(() => {
+                        location.reload(true);
+                    });
+                };
+
+                refresh();
             }
 
             const error = (response && response.message || response.error.message);
